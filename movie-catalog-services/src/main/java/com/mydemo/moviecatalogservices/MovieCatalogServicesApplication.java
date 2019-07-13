1f3d237d29b0dc31aa71ccfe6c85bb404f1efb84
DELL <DELL@DELL-PC>
2019-07-13 20:38:37 +0530

    Added Eureka Changes.

--- movie-catalog-services/src/main/java/com/mydemo/moviecatalogservices/MovieCatalogServicesApplication.java
+++ movie-catalog-services/src/main/java/com/mydemo/moviecatalogservices/MovieCatalogServicesApplication.java
@@ -2,10 +2,12 @@ package com.mydemo.moviecatalogservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServicesApplication {
 
 	public static void main(String[] args) {
--- rating-info-services/src/main/java/com/mydemo/ratinginfoservices/RatingInfoServicesApplication.java
+++ rating-info-services/src/main/java/com/mydemo/ratinginfoservices/RatingInfoServicesApplication.java
@@ -2,8 +2,10 @@ package com.mydemo.ratinginfoservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingInfoServicesApplication {
 
 	public static void main(String[] args) {
